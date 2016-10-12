2864784efa6171e11b43d99a533731ffa239b734
Sebastian Reimers <info@miete-admin.de>
2016-10-12 14:28:26 +0200

    fix JUCE 4.2

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-cd JUCE/extras/Introjucer/Builds/MacOSX/
+cd JUCE/extras/Projucer/Builds/MacOSX/
 xcodebuild -configuration Release
 cd ../../../../../
 
-JUCE/extras/Introjucer/Builds/MacOSX/build/Release/Introjucer.app/Contents/MacOS/Introjucer --resave StudioLink/StudioLink.jucer
+JUCE/extras/Projucer/Builds/MacOSX/build/Release/Projucer.app/Contents/MacOS/Projucer --resave StudioLink/StudioLink.jucer
 
 cd StudioLink/Builds/MacOSX/
 xcodebuild -configuration Release -xcconfig ../../../build.xconfig
