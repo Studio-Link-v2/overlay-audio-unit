a798d5d84fe1f88feb3ed4081583c636577737da
Sebastian Reimers <hallo@studio-link.de>
2019-09-01 16:44:58 +0200

    update for JUCE 5.x

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-cd JUCE/extras/Projucer/Builds/MacOSX/
-xcodebuild -configuration Release
-cd ../../../../../
+#cd JUCE/extras/Projucer/Builds/MacOSX/
+#xcodebuild -configuration Release
+#cd ../../../../../
 
-JUCE/extras/Projucer/Builds/MacOSX/build/Release/Projucer.app/Contents/MacOS/Projucer --resave StudioLink/StudioLink.jucer
+#JUCE/extras/Projucer/Builds/MacOSX/build/Release/Projucer.app/Contents/MacOS/Projucer --resave StudioLink/StudioLink.jucer
+JUCE/Projucer.app/Contents/MacOS/Projucer --resave StudioLink/StudioLink.jucer
 
-cd StudioLink/Builds/MacOSX/
+pushd StudioLink/Builds/MacOSX/
 xcodebuild -configuration Release -xcconfig ../../../build.xconfig
-cd ../../../
+popd
